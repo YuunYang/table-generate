e65fafc6dd54faf40e4ea7eb8841675c8cef081d
yukun <yukun@xsky.com>
2019-11-07 16:11:47 +0800

    generate build

---
+++ build/precache-manifest.e9787b902c8c0be1ae107baf6b96c2c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3f900c5af0b69d441d78406ee70691",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d79fc6aaade163f83b04",
+    "url": "/static/css/main.eda9c7ff.chunk.css"
+  },
+  {
+    "revision": "75f471d554812cd01c1b",
+    "url": "/static/js/2.5fa653e8.chunk.js"
+  },
+  {
+    "revision": "d79fc6aaade163f83b04",
+    "url": "/static/js/main.757ece42.chunk.js"
+  },
+  {
+    "revision": "55f25f5920508f2d5774",
+    "url": "/static/js/runtime-main.bebb0c67.js"
+  }
+]);
